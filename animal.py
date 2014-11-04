24414e97c1eeadbf629f84bcd63d21c81dd8ab30
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-11-04 23:34:03 +0200

    Changed self.age to months. Fixed grow method

--- animal.py
+++ animal.py
@@ -5,7 +5,7 @@ class Animal:
 
     def __init__(self, species, age, name, gender, weight):
         self.species = species
-        self.age = age
+        self.age = age * 12
         self.name = name
         self.gender = gender
         self.weight = weight
@@ -16,14 +16,14 @@ class Animal:
         #how much food the animal eats in kilos
         #and it can be multiply by the cost of eaten food so we will have the outcomes
         return self.weight * self.animal_settings.get_food_weight(self.species)
-        
+
     def grows(self):
         self.age += 1
         if (self.weight) < (
                 self.animal_settings.get_average_weight(self.species)):
-            self.weight += self.age * self.animal_settings.get_weight_age(self.species)
+            self.weight += self.animal_settings.get_weight_age(self.species)
 
     def chance_of_dying(self):
         return (
-            self.age / self.animal_settings.get_life_expectancy(self.species)
+            (self.age / 12) / self.animal_settings.get_life_expectancy(self.species)
         )
