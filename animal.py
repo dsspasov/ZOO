2d7583e41380f73944a26e29416e551b8541ee8b
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-11-04 23:44:31 +0200

    added method die

--- animal.py
+++ animal.py
@@ -3,7 +3,7 @@ from get_json_data import GetJsonData
 
 class Animal:
 
-    def __init__(self, species, age, name, gender, weight):
+    def __init__(self, species, name, gender, age, weight):
         self.species = species
         self.age = age * 12
         self.name = name
@@ -11,10 +11,9 @@ class Animal:
         self.weight = weight
         self.animal_settings = GetJsonData("database.json")
 
+    #how much food the animal eats in kilos
+    #and it can be multiply by the cost of eaten food so we will have the outcomes
     def eat(self):
-        pass
-        #how much food the animal eats in kilos
-        #and it can be multiply by the cost of eaten food so we will have the outcomes
         return self.weight * self.animal_settings.get_food_weight(self.species)
 
     def grows(self):
@@ -22,6 +21,12 @@ class Animal:
         if (self.weight) < (
                 self.animal_settings.get_average_weight(self.species)):
             self.weight += self.animal_settings.get_weight_age(self.species)
+
+    #We could add dead before max age reached
+    def die(self):
+        if self.chance_of_dying >= 1:
+            return True
+        return False
 
     def chance_of_dying(self):
         return (
