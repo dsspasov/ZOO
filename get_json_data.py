30ad357713ca834fe8c14f44df449a1e533b74c7
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-11-04 21:20:42 +0200

    changes to json

--- get_json_data.py
+++ get_json_data.py
@@ -19,18 +19,23 @@ class GetJsonData:
             list_of_keys.append(key)
         return list_of_keys
 
+    #returns years
     def get_life_expectancy(self, type_of_animal):
         return self.result[type_of_animal]["life_expectancy"]
 
+    #returns carnivore or herbivore
     def get_food_type(self, type_of_animal):
         return self.result[type_of_animal]["food_type"]
 
+    #returns months
     def get_gestation_period(self, type_of_animal):
         return self.result[type_of_animal]["gestation_period"]
 
+    #returns weight in kilos
     def get_newborn_weight(self, type_of_animal):
         return self.result[type_of_animal]["newborn_weight"]
 
+    #returns gained weight for one month in kilo
     def get_weight_age(self, type_of_animal):
         return self.result[type_of_animal]["weight_age"]
 
