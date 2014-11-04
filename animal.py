f07da2b5cb63e511e975320b61c8afd553893967
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-11-05 00:45:46 +0200

    changed animal_settings to info for shortness

--- animal.py
+++ animal.py
@@ -9,19 +9,19 @@ class Animal:
         self.name = name
         self.gender = gender
         self.weight = weight
-        self.animal_settings = GetJsonData("database.json")
+        self.info = GetJsonData("database.json")
 
     #how much food the animal eats in kilos
     #and it can be multiply by the cost of eaten food so we will have the outcomes
     def eat(self):
-        return self.weight * self.animal_settings.get_food_weight(self.species)
+        return self.weight * self.info.get_food_weight(self.species)
 
     def grows(self):
         self.age += 1
-        if self.weight < self.animal_settings.get_average_weight(self.species):
-            self.weight += self.animal_settings.get_weight_age(self.species)
-            if self.weight > self.animal_settings.get_average_weight(self.species):
-                self.weight = self.animal_settings.get_average_weight(self.species)
+        if self.weight < self.info.get_average_weight(self.species):
+            self.weight += self.info.get_weight_age(self.species)
+            if self.weight > self.info.get_average_weight(self.species):
+                self.weight = self.info.get_average_weight(self.species)
 
     #We could add dead before max age reached
     def die(self):
@@ -31,5 +31,5 @@ class Animal:
 
     def chance_of_dying(self):
         return (
-            (self.age / 12) / self.animal_settings.get_life_expectancy(self.species)
+            (self.age / 12) / self.info.get_life_expectancy(self.species)
         )
