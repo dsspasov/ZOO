01a7a762e8a8b8203ff5ad0ba610f9c145d31261
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-11-09 13:23:07 +0200

    accomodate animal test fixed

--- zoo_class_tests.py
+++ zoo_class_tests.py
@@ -6,10 +6,12 @@ from zoo_class import Zoo
 class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zz = Zoo(10, 10000)
-        self.male_kitten = Animal("tiger", "DiviaLud", "male", 10, 150)
-        self.female_kitten = Animal("tiger", "DiviaLuda", "female", 10, 150)
+        self.male_kitten = Animal('tiger', 'DiviaLud', 'male', 10, 150)
+        self.female_kitten = Animal('tiger', 'DiviaLuda', 'female', 10, 150)
+        self.female_ape = Animal('ape', 'Chico', 'female', 15, 35)
         self.zz.animals.append(self.male_kitten)
         self.zz.animals.append(self.female_kitten)
+        self.zz.animals.append(self.female_ape)
 
     def test_init(self):
         self.assertIn(self.male_kitten, self.zz.animals)
@@ -18,8 +20,10 @@ class TestZoo(unittest.TestCase):
         self.assertNotEqual(self.zz.animals, ['panda'])
 
     def test_accomodate_animal(self):
-        self.zz.accomodate_animal(self.male_kitten)
-        self.assertIn(self.male_kitten, self.zz.animals)
+        self.male_tiger = Animal('tiger', 'tiger', 'male', 10, 150)
+        self.assertRaises(ValueError, self.zz.accomodate_animal, (self.male_kitten))
+        self.zz.accomodate_animal(self.male_tiger)
+        self.assertIn(self.male_tiger, self.zz.animals)
 
     def test_win_money(self):
         before_money = self.zz.budget
@@ -27,17 +31,21 @@ class TestZoo(unittest.TestCase):
         after_money = self.zz.budget
         self.assertGreaterEqual(after_money, before_money)
 
-    def test_make_animals(self):
-        self.zz.make_animals(self.male_kitten, self.female_kitten)
-        self.assertTrue(self.female_kitten.pregnant)
+    def test_are_same_species(self):
+        self.assertTrue(self.zz.are_same_species([self.male_kitten, self.female_kitten]))
+        self.assertFalse(self.zz.are_same_species([self.male_kitten, self.female_ape]))
 
-    def test_born_animal(self):
-        self.female_kitten.pregnant = True
-        self.female_kitten.pregnancy = self.female_kitten.info.gestation_period
-        self.assertTrue(self.zz.born_animal)
+    # def test_make_animals(self):
+    #     self.zz.make_animals(self.male_kitten, self.female_kitten)
+    #     self.assertTrue(self.female_kitten.pregnant)
+
+    # def test_born_animal(self):
+    #     self.female_kitten.pregnant = True
+    #     self.female_kitten.pregnancy = self.female_kitten.info.gestation_period
+    #     self.assertTrue(self.zz.born_animal)
 
     def test_get_food_type(self):
-        self.assertEqual(self.male_kitten.info.food_type(), "carnivore")
+        self.assertEqual(self.male_kitten.info.food_type(), 'carnivore')
 
     def test_have_money(self):
         before_pay = self.zz.budget
